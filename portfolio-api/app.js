6b48216b6a82314e04fc09a66c8f74bdd11ac627
connormayfield <connormayfield@gmail.com>
2019-10-03 17:50:37 -0500

    version 1.0.0

--- portfolio-api/app.js
+++ portfolio-api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const sendGrid = require("@sendGrid/mail");
+const path = require("path");
 
 const app = express();
 
@@ -53,4 +54,17 @@ app.post("/api/email", (req, res, next) => {
     });
 });
 
-app.listen(3030, "0.0.0.0");
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
