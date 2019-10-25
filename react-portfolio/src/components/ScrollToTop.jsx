721eada215a266edb1424e28aa2375cee3806668
connormayfield <connormayfield@gmail.com>
2019-10-25 17:24:40 -0500

    footer

--- react-portfolio/src/components/ScrollToTop.jsx
+++ react-portfolio/src/components/ScrollToTop.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
+// import React from "react";
+// import { TinyButton as ScrollUpButton } from "react-scroll-up-button";
 
-export default class Index extends React.Component {
-  render() {
-    return (
-      <div className="scrollUpButton">
-        <ScrollUpButton ShowAtPosition={2500} />
-      </div>
-    );
-  }
-}
+// export default class Index extends React.Component {
+//   render() {
+//     return (
+//       <div className="scrollUpButton">
+//         <ScrollUpButton ShowAtPosition={2500} />
+//       </div>
+//     );
+//   }
+// }
