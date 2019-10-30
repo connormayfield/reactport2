df85670be333689ba83fe0b585501a8ade53ce45
connormayfield <connormayfield@gmail.com>
2019-10-30 16:56:00 -0500

    Bottom svg width fix

--- react-portfolio/src/pages/ContactPage.jsx
+++ react-portfolio/src/pages/ContactPage.jsx
@@ -138,8 +138,7 @@ class ContactPage extends React.Component {
           </Col>
         </Row>
         <svg
-          width="100%"
-          className="contactSvg"
+          className="bottomContactSvg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
         >
